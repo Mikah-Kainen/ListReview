ecaeb6f85ef5a77ae32c03ece0687141e42deb95
Mikah Kainen <mikah@mltd.com>
2020-06-08 15:42:18 -0700

    initial commit

---
+++ ListReview/MyList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListReview
+{
+    public class MyList<T>
+    {
+
+        private const int initialCapacity = 10;
+        private T[] List = new T[initialCapacity];
+        public int Size = 0;
+
+        public void Add(T thingToAdd)
+        {
+            List[Size] = thingToAdd;
+            Size++;
+            InsureCapacity();
+        }
+
+        private void InsureCapacity()
+        {
+            int listCapacity = List.Length;
+            if (Size < listCapacity)
+            {
+                return;
+            }
+            
+            T[] tempList = new T[Size * 2];
+            for(int i = 0; i < Size; i ++)
+            {
+                tempList[i] = List[i];
+            }
+            List = tempList;
+        }
+
+        public T FindIndex(int targetIndex)
+        {
+            return List[targetIndex];
+        }
+
+        public void ChangeValue(int indexToChange, T newValue)
+        {
+            List[indexToChange] = newValue;
+        }
+
+        public void Sub(T targetValue)
+        {
+            int targetIndex = FindValue(targetValue);
+            for(int i = targetIndex; i < Size; i ++)
+            {
+                List[i] = List[i + 1];
+            }
+        }
+
+        private int FindValue(T targetValue)
+        {
+            for(int i = 0; i < Size; i ++)
+            {
+                if(List[i].Equals(targetValue))
+                {
+                    return i;
+                }
+            }
+            throw new IndexOutOfRangeException("that number wasn't found");
+        }
+    }
+}
---
+++ ListReview/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ListReview
+{
+    class Program
+    {
+        static MyList<int> BubbleSort(MyList<int> numbersToSort)
+        {
+            int tempHolder;
+            int numberOfSwitches;
+
+            for (int i = 0; i < numbersToSort.Size - 1; i ++)
+            {
+                numberOfSwitches = numbersToSort.Size;
+                for (int x = 0; x < numbersToSort.Size - 1; x++)
+                {
+                    if (numbersToSort.FindIndex(x) > numbersToSort.FindIndex(x + 1))
+                    {
+                        tempHolder = numbersToSort.FindIndex(x);
+                        numbersToSort.ChangeValue(x, numbersToSort.FindIndex(x + 1));
+                        numbersToSort.ChangeValue(x + 1, tempHolder);
+                    }
+                    else
+                    {
+                        numberOfSwitches--;
+                    }
+                }
+
+                if (numberOfSwitches == 0)
+                {
+                    return numbersToSort;
+                }
+            }
+    
+            return numbersToSort;
+        }
+
+        static void Main(string[] args)
+        {
+            MyList<int> numbers = new MyList<int>();
+            Random random = new Random();
+
+            for(int i = 0; i < 10; i ++)
+            {
+                numbers.Add(random.Next(0, 30));
+                Console.WriteLine($"{numbers.FindIndex(i)}");
+            }
+            Console.WriteLine();
+            numbers = BubbleSort(numbers);
+            for(int i = 0; i < 10; i ++)
+            {
+                Console.WriteLine($"{numbers.FindIndex(i)}");
+            }
+        }
+    }
+}
